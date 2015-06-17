1a3bb95e7a912134fc98cf5f5f7a0ab7cd64e2f7
Renan Samuel da Silva <uber.renan@gmail.com>
2015-06-17 09:58:27 -0300

    Some tests

--- main.c
+++ main.c
@@ -8,19 +8,43 @@ int main(){
     srand(time(NULL));
     _queue *q;
 
+    int **tsp;
+    int i, j;
+    int n;
+
+    //n = 5;
+
+    scanf("%d", &n);
+
+    tsp = (int**) malloc (sizeof(int*)*n);
+
+    for (i=0; i<n; i++){
+        tsp[i] = (int*) malloc (sizeof(int)*n);
+    }
+
+    for (i=0; i<n; i++){
+        for (j=0; j<n; j++){
+            scanf("%d", &tsp[i][j]);
+        }
+    }
+
+    for (i=0; i<n; i++){
+        for (j=0; j<n; j++){
+            printf("%2.d ", tsp[i][j]);
+        }
+        puts("");
+    }
+
     q = queue_init();
+    queue_insert(q, 0);
+    queue_insert(q, 0);
+    queue_insert(q, 0);
 
-    int i;
+    _restricao *r;
+    r = restricao_init(1, 2);
+    restricao_insert(r, 2, 3);
 
-    for (i=0; i<30; i++){
-        queue_insert(q, rand()%10);
-    }
-
-    queue_print(q);
-
-    for (i=0; i<10;i++){
-        printf("%d\n", queue_remove(q));
-    }
+    printf("%d %d %d %d\n", r->s, r->t, r->next->s, r->next->t);
 
     return EXIT_SUCCESS;
 }
